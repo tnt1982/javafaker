0c475ff2744846e2e9c7e318a235677d4c7abb25
tnt1982 <97390681+tnt1982@users.noreply.github.com>
2022-04-02 15:16:13 +0200

    faker, losowe adresy email;

--- src/main/java/Faker.java
+++ src/main/java/Faker.java
@@ -1,17 +1,25 @@
 import com.github.javafaker.Faker;
 import com.github.javafaker.Dog;
+import com.github.javafaker.Name;
 import java.util.Locale;
 
 class DataGenerator {
     public static void main(String[] args) {
-        Faker faker = new Faker(new Locale("pl-PL"));
+        Faker faker = new Faker(new Locale("pl"));
 
-        Dog dog = faker.dog();
-        String dogbreed = faker.dog().breed();
-        System.out.println("Losowe rasy psów:");
-        final int randomDataSize = 10;
-        for (int i = 0; i < randomDataSize; i++) {
-            System.out.println("Rasa psa: " + dog.breed() +" " + dog.age() + " " + dog.name());
+//        Dog dog = faker.dog();
+//        String dogbreed = faker.dog().breed();
+//        System.out.println("Losowe rasy psów:");
+//        final int randomDataSize = 10;
+//        for (int i = 0; i < randomDataSize; i++) {
+//            System.out.println("Rasa psa: " + dog.breed() +" " + dog.age() + " " + dog.name());
+//        }
+
+        Name name = faker.name();
+        System.out.println("Losowe adresy email:");
+        final int randomDataSize = 77;
+        for (int i = 0; i < randomDataSize; i ++) {
+            System.out.println((name.firstName() + "." + name.lastName() + "@gmail.com").toLowerCase());
         }
     }
 }
